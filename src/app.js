3439f76a5e0c59efbd148f01d46b33d687b27f17
Lellien <ibexcomplex@gmail.com>
2021-11-28 20:10:12 +0000

    Added API call using geolocation

--- src/app.js
+++ src/app.js
@@ -272,4 +272,28 @@ celsiusButton.addEventListener("change", showCelsius);
 let fahrenheitButton = document.querySelector("#btnradio2");
 fahrenheitButton.addEventListener("change", showFahrenheit);
 
+//current location
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+
+  let unit = checkUnit();
+
+  let apiKey = "0392c3c6a728319e4bcd5bed20b65b72";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(getCurrentCity);
+
+  function getCurrentCity(response) {
+    formatCity(response.data);
+  }
+}
+
+function getGeolocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getGeolocation);
+
 search("London");
